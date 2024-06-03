0fa507d292765adaa89248abbe0360a8a31e476d
Brooke Joseph <brookecoco123123@gmail.com>
2024-06-03 08:49:55 -0400

    spelling and polish

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
       <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', marginBottom: '30px' }}>
         <div style={{ textAlign: 'center', maxWidth: '800px', wordWrap: 'break-word', padding: '0 20px' }}>
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>
-            Previous technologies/endeavors: computer vision, federated learning, secure aggregation, differential privacy, secure multiparty computation, knowledge graphs for information overload, CNNs for silicon wafer detection, differential privacy for more private secure data, CNNs trained using federated learning.
+            Previous Endeavors: Computer Vision, Federated Learning, Secure Aggregation, Differential Privacy, Secure Multiparty Computation, Knowledge Graphs for Information Overload, CNNs for Silicon Wafer Detection.
           </p>
         </div>
       </div>
@@ -74,11 +74,11 @@ function App() {
         <h2 style={{ fontFamily: 'Times New Roman', textAlign: 'center', fontSize: '2em', color: '#C39BD3' }}>Experiences</h2>
         <div style={{ marginLeft: '20px', wordWrap: 'break-word' }}>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3'}}>Stealth Startup - Toronto</h3>
-          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Working with previous founders at Maluuba and Wish. Helped build different apps with a total of 100k users in one month.</p>
+          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Working with previous founders at Maluuba and Wish. Helping build different apps currently helped reach 100k users in one month.</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Devron - New York</h3>
-          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Collaborated with senior engineers to incorporate differential privacy into their platform. This involved navigating and understanding their complex infrastructure to ensure effective implementation.</p>
+          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Collaborated with senior engineers to incorporate Differential Privacy into their platform. This involved navigating and understanding their complex infrastructure to ensure effective implementation.</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3'}}>TensoAI - Montreal</h3>
-          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Researched how agricultural data from satellites and sensors could train machine learning models using federated learning. I also investigated and recommended strategies for setting up the company's federated learning infrastructure to enhance data privacy and collaborative model training.</p>
+          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Researched how agricultural data from satellites and sensors could train machine learning models using Federated Learning. I also investigated and recommended strategies for setting up the company's Federated Learning infrastructure to enhance data privacy and collaborative model training.</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3'}}>Dr Tong's Biochemistry Lab</h3>
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>As the youngest research assistant in the lab, I revamped the website and collaborated with PhDs to code 3D models of various proteins using PyMol, significantly boosting productivity by 10x.</p>
         </div>
@@ -95,9 +95,11 @@ function App() {
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Multi-GPU Training</h3>
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Worked with a PhD at MIT investigating how to train a CNN on medical data for disease prediction.</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Differential Privacy Research</h3>
-          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Invited to give presentations all over the world (Germany, Toronto, Washington DC, etc.) on my research in Differential Privacy. Specifically looking at ways to balance the privacy-utility tradeoff, with over 80,000 people in attendance across all conferences.</p>
+          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Invited to give presentations all over the world (Germany, Toronto, Washington DC, etc.) on my research in Differential Privacy. Specifically looking at ways to balance the privacy-utility tradeoff. Over 80,000 people in attendance across all conferences.</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>International Marathon Runner</h3>
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Travelled all over to compete in half and full marathons, ranking top 20 in my age group for the Detroit Marathon.</p>
+          <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Google WomenTechMaker</h3>
+          <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Selected by Google to help mentor women in tech, orgianze Google events and invited to speak on research.</p>
         </div>
       </div>
       <div style={{ width: '100%', maxWidth: '1100px' }}>
