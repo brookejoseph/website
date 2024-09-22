4cfdf80839cd11937831433cd4689c5cef826821
Brooke Joseph <brookecoco123123@gmail.com>
2024-09-22 11:42:59 -0400

    fixed videos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
+
 
 function App() {
+  const imageUrls = [
+    '/pictures/app.png',
+    'pictures/burrow.png',
+    'pictures/cables.png',
+    'pictures/gartner.png',
+    'pictures/robot.png',
+    'pictures/womenofin.png',
+  ];
+
+
+
   return (
     <div style={{
       display: 'flex',
@@ -66,7 +79,7 @@ function App() {
       <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', marginBottom: '30px' }}>
         <div style={{ textAlign: 'center', maxWidth: '800px', wordWrap: 'break-word', padding: '0 20px' }}>
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>
-            Previous Endeavors: Computer Vision, Federated Learning, Secure Aggregation, Differential Privacy, Secure Multiparty Computation, Knowledge Graphs for Information Overload, CNNs for Silicon Wafer Detection.
+            Previous Endeavors: Computer Vision, Programming microcontrollers, control algorithms, CAD designs, Federated Learning, Secure Aggregation, Differential Privacy, Knowledge Graphs, CNNs for Silicon Wafer Detection.
           </p>
         </div>
       </div>
@@ -86,6 +99,8 @@ function App() {
       <div style={{ width: '100%', maxWidth: '800px', marginBottom: '30px' }}>
         <h2 style={{ fontFamily: 'Times New Roman', textAlign: 'center', fontSize: '2em', color: '#C39BD3' }}>Other Cool Stuff</h2>
         <div style={{ marginLeft: '20px', wordWrap: 'break-word' }}>
+        <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Building with ROS/MATLAB</h3>
+        <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>I use Simulink in MATLAB to program robots for executing various actions in simulated environments. For fun mainly :)</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Presented at Google</h3>
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>I was invited to give a presentation on my projects and research in Federated Learning. Specifically looking at how to incorporate Differential Privacy and the challenges with that.</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Created Multimodal Knowledge Graphs</h3>
@@ -98,7 +113,7 @@ function App() {
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Invited to give presentations all over the world (Germany, Toronto, Washington DC, etc.) on my research in Differential Privacy. Over 80,000 people in attendance across all conferences.</p>
           <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>International Marathon Runner</h3>
           <p style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>Travelled all over to compete in half and full marathons, ranking top 20 in my age group for the Detroit Marathon.</p>
-          <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>App Developement</h3>
+          <h3 style={{ fontFamily: 'Times New Roman', fontSize: '1.5em', color: '#C39BD3' }}>Projects</h3>
           <ul style={{ fontFamily: 'Times New Roman', fontSize: '1.2em' }}>
             <li>Contributed to the development of a voice assistant app. My role involved fine-tuning models and integrating text-to-speech technology. (<a href="https://mymemoirs.app" target="_blank" rel="noopener noreferrer">mymemoirs.app</a>)</li>
             <li>Recreated character AI with alternating moods. (<a href="https://trycarson.com" target="_blank" rel="noopener noreferrer">trycarson.com</a>)</li>
@@ -114,20 +129,18 @@ function App() {
       <div style={{ width: '100%', maxWidth: '1100px' }}>
         <h2 style={{ fontFamily: 'Times New Roman', textAlign: 'center', fontSize: '2em', color: '#C39BD3' }}>Videos</h2>
         <div style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
-          {['https://youtube.com/embed/UXxHawXiN6w', 'https://www.youtube.com/embed/75wv-WgqTic', 'https://www.youtube.com/embed/t18MBKs5uSQ', 'https://www.youtube.com/embed/S3PsPC8kHng'].map((src, index) => (
-            <iframe
-              key={index}
-              style={{ margin: '10px' }}
-              width="45%"
-              height="315"
-              src={src}
-              title={`Video ${index + 1}`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          ))}
-        </div>
+      {['https://youtube.com/embed/UXxHawXiN6w', 'https://www.youtube.com/embed/75wv-WgqTic', 'https://www.youtube.com/embed/t18MBKs5uSQ', 'https://www.youtube.com/embed/S3PsPC8kHng'].map((src, index) => (
+        <iframe
+          key={index}
+          className="responsive-iframe"
+          src={src}
+          title={`Video ${index + 1}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      ))}
+    </div>
       </div>
     </div>
   );
